c891bc33b08a749111d2298c66fc9b25d43e29e5
gsalma99 <salmagonzalezzz10011@gmail.com>
2022-11-10 22:00:59 -0600

    updated routes.js

---
+++ routes/api-routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+const db = require('../utils/db');
+
+router.post('/notes', (req, res) => {
+  db.addNotes(req.body).then(notes => res.json(notes)).catch(error => console.log(error));
+})
+
+router.get('/notes', (req, res) => {
+console.log(db);
+    db.getNotes().then(data => {
+        res.json(JSON.parse(data))
+    })
+})
+
+
+module.exports = router;
